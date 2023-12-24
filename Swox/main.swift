ca3d16a08b268b2ec3c7ca6785f92048e575cde8
Kaho Yeung <dev@kahoyeung.com>
2023-12-24 13:22:36 +0000

    updated cli args parsing

--- Swox/main.swift
+++ Swox/main.swift
@@ -9,31 +9,49 @@ import Foundation
 import SwoxLib
 
 /*
-Default port is 1080.
-Use arg "-p" for a different port number.
+    Default port is 1080.
+    
+    Use arg "-p" for a different port number, "-f" to enable TCP Fast Open
 
-Example:
-`Swox -p 1088`
+    Example:
+    `Swox -p 1088`
+    
  */
 
 var port: UInt16 = 1080
+var tfoEnabled = false
 
-let args = CommandLine.arguments
+enum ArgParsingState {
+    case searching, expectingPortNumber
+}
 
-for i in args.indices {
-    if args[i] == "-p", i < args.count - 1 {
-        guard let overridePort = UInt16(args[i + 1]) else {
-            print("'\(args[i + 1])'is not a valid port number")
-            break
+var state = ArgParsingState.searching
+
+for arg in CommandLine.arguments {
+    switch state {
+    case .searching:
+        switch arg {
+        case "-p":
+            state = .expectingPortNumber
+        case "-f":
+            tfoEnabled = true
+        default:
+            print("invalid argument: \(arg)")
+            continue
+        }
+    case .expectingPortNumber:
+        guard let overridePort = UInt16(arg) else {
+            print("'\(arg)'is not a valid port number")
+            exit(1)
         }
         port = overridePort
-        break
     }
 }
 
-print("Starting Swox on port \(port)")
+print("Starting Swox on port \(port)...")
+print("TCP Fast Open enabled: \(tfoEnabled)")
 
-let server = try! SwoxSocks5Server(port: port)
+let server = try! SwoxSocks5Server(port: port, tcpFastOpen: tfoEnabled)
 server.start()
 
 RunLoop.main.run()
